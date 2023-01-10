e650c336646c432450373a6694902333157b4a1a
IrynaShashko <irishkashashko@gmail.com>
2023-01-10 20:12:22 +0200

    add post card

--- Screens/CreatePostsScreen.js
+++ Screens/CreatePostsScreen.js
@@ -1,10 +1,26 @@
 import { Camera, CameraType } from "expo-camera";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
-export default function App() {
+const initialState = {
+  text: "",
+  location: "",
+  photo: "",
+};
+
+export default function CreatePostsScreen({ navigation }) {
+  const [state, setState] = useState(initialState);
   const [camera, setCamera] = useState(null);
   const [photo, setPhoto] = useState(null);
   const [type, setType] = useState(CameraType.back);
@@ -34,18 +50,31 @@ export default function App() {
   const takePhoto = async () => {
     const photo = await camera.takePictureAsync();
     setPhoto(photo.uri);
+    setState((prevState) => ({ ...prevState, photo: photo.uri }));
   };
 
-  console.log("photo----->", photo);
+  const sendPhoto = () => {
+    navigation.navigate("Posts", state);
+    setState(initialState);
+    setPhoto(null);
+  };
 
   return (
     <View style={styles.container}>
       <Camera style={styles.camera} type={type} ref={setCamera}>
+        {photo && (
+          <View style={styles.takePhotoContainer}>
+            <Image
+              style={{ height: 238, width: 380, borderRadius: 8 }}
+              source={{ uri: photo }}
+            />
+          </View>
+        )}
         <View style={styles.toggleContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+          <TouchableOpacity style={styles.toggle} onPress={toggleCameraType}>
             <MaterialCommunityIcons
               name="camera-flip"
-              size={24}
+              size={30}
               color="#e8e8e8"
             />
           </TouchableOpacity>
@@ -56,17 +85,52 @@ export default function App() {
           </TouchableOpacity>
         </View>
       </Camera>
+      <TextInput
+        style={{ fontSize: 14, paddingTop: 10, paddingBottom: 10 }}
+        placeholder="Загрузіть фото"
+      />
+      <TextInput
+        style={styles.input}
+        value={state.text}
+        placeholder="Назва..."
+        onChangeText={(value) =>
+          setState((prevState) => ({ ...prevState, text: value }))
+        }
+      />
+      <View style={{ position: "relative" }}>
+        <View style={styles.location}>
+          <AntDesign name="enviromento" size={16} color="#bdbdbd" />
+          <TextInput
+            value={state.location}
+            placeholder="Місцевість..."
+            onChangeText={(value) =>
+              setState((prevState) => ({ ...prevState, location: value }))
+            }
+          />
+        </View>
+      </View>
+      <View style={styles.submitButton}>
+        <Button onPress={sendPhoto} title="Опублікувати" color={"#bdbdbd"} />
+      </View>
+      <TouchableOpacity
+        // onFocus={() => setPhoto(null)}
+        style={styles.deleteButton}
+      >
+        <MaterialIcons name="delete-outline" size={24} color="#dadada" />
+      </TouchableOpacity>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
+    flex: 1,
     paddingHorizontal: 16,
+    backgroundColor: "#fff",
   },
   camera: {
     position: "relative",
     alignItems: "center",
+    justifyContent: "center",
     height: 240,
     marginTop: 50,
     borderWidth: 1,
@@ -78,7 +142,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     backgroundColor: "transparent",
-    margin: 160,
+    // margin: 170,
+    borderRadius: 8,
   },
   toggleContainer: {
     position: "absolute",
@@ -94,5 +159,49 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#fff",
+  },
+  toggle: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    height: 50,
+    borderColor: "#E8E8E8",
+    borderBottomWidth: 1,
+    backgroundColor: "#fff",
+    marginBottom: 16,
+  },
+  submitButton: {
+    backgroundColor: "#f6f6f6",
+    borderRadius: 50,
+    paddingVertical: 8,
+  },
+  location: {
+    flexDirection: "row",
+    alignItems: "baseline",
+    height: 35,
+    borderColor: "#E8E8E8",
+    borderBottomWidth: 1,
+    backgroundColor: "#fff",
+    marginBottom: 32,
+  },
+  takePhotoContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    borderWidth: 1,
+    borderColor: "#fff",
+    // height: 240,
+  },
+  deleteButton: {
+    position: "absolute",
+    bottom: 20,
+    justifyContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+    width: 70,
+    height: 40,
+    backgroundColor: "#f6f6f6",
+    borderRadius: 20,
   },
 });
--- Screens/PostsScreen.js
+++ Screens/PostsScreen.js
@@ -1,12 +1,58 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+import { EvilIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
-export default function PostsScreen({ navigation }) {
+export default function PostsScreen({ navigation, route }) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    if (route.params) {
+      setPosts((prevState) => [...prevState, route.params]);
+    }
+  }, [route.params]);
+
+  console.log("posts", posts);
+
   return (
     <View style={styles.container}>
       <MaterialIcons name="logout" size={24} color="#BDBDBD" />
-      <Text>PostsScreen</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={{ marginBottom: 20 }}>
+            <Image
+              source={{ uri: item.photo }}
+              style={{ height: 200, marginHorizontal: 20, borderRadius: 8 }}
+            />
+            <Text style={{ marginLeft: 20, fontSize: 16 }}>{item.text}</Text>
+            <View
+              style={{
+                paddingHorizontal: 20,
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <TouchableOpacity>
+                <EvilIcons name="comment" size={24} color="#bdbdbd" />
+              </TouchableOpacity>
+              <Text style={{ fontSize: 16 }}>
+                <AntDesign name="enviromento" size={18} color="#bdbdbd" />
+                {item.location}
+              </Text>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -15,6 +61,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
   },
 });
